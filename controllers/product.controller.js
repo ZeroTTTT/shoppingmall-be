8955166b500bd1e9a82138e3ab29d6e24cd73ea7
Youngho Park <dudgh4651@gmail.com>
2024-06-04 21:27:34 +0900

    17. 페이지네이션

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,4 +1,6 @@
 const Product = require("../models/Product");
+
+const PAGE_SIZE=5;
 const productController = {};
 
 productController.createProduct = async (req,res) =>{
@@ -36,9 +38,23 @@ productController.getProducts = async (req, res) => {
         const {page, name}= req.query
         const cond = name?{name:{$regex:name, $options:'i'}}:{}
         let query = Product.find(cond)
+        let response = { status:'success'}  //response 객체로 만든이유는 필요한값 추가로 넣어서 리턴줄 수 있기 때문
+        if(page){
+            query.skip((page-1)*PAGE_SIZE).limit(PAGE_SIZE);
+            //최종 몇개페이지인지?
+            //전체 페이지 개수 = 전체데이터 개수 / 페이지 사이즈
+
+            //데이터 총 개수 
+            const totalItemNum = await Product.find(cond).count();
+            //데이터 총 개수 / PAGE_SIZE
+            const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+            response.totalPageNum = totalPageNum;
+        }
 
         const productList = await query.exec();
-        res.status(200).json({status:'success', data:productList});
+        response.data = productList;
+        // res.status(200).json({status:'success', data:productList});
+        res.status(200).json(response);
     } catch (error) {
         res.status(400).json({status:'fail', error: error.message});
     }
